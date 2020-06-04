c96b659a8e281b9c6e0f5ecd6e7e9b4c82672a8c
Your name here <khoitr190@gmail.com>
2020-06-04 21:13:36 +0700

    lesson Conditional Rendering

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: '#app',
     data:{
         product: 'Socks',
-        image: 'kyrie_white.jpg' //image data, looke careful file index.html line <img class... v-bind:src="image" /> image it same image data
+        image: 'kyrie_white.jpg', //image data, looke careful file index.html line <img class... v-bind:src="image" /> image it same image data
         //Name: '.jpg or .png' (1)
+        colorSocks: true
     }
 })
