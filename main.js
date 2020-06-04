10a6f01348729a72ea5444492eefc19820bc83e9
Your name here <khoitr190@gmail.com>
2020-06-04 11:25:08 +0700

    lesson Attribute Binding Vuejs

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var app = new Vue({
     el: '#app',
     data:{
-        product: 'Socks'
+        product: 'Socks',
+        image: 'kyrie_white.jpg' //image data, looke careful file index.html line <img class... v-bind:src="image" /> image it same image data
+        //Name: '.jpg or .png' (1)
     }
 })
