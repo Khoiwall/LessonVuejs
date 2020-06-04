57a7c011fa2996bfebb3f7b1c5f474832fabbd60
Your name here <khoitr190@gmail.com>
2020-06-04 21:36:11 +0700

    Lesson List Rendering

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ var app = new Vue({
         product: 'Socks',
         image: 'kyrie_white.jpg', //image data, looke careful file index.html line <img class... v-bind:src="image" /> image it same image data
         //Name: '.jpg or .png' (1)
-        colorSocks: true
+        colorSocks: true,
+        infos: ["80 cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 1,
+                variantColor: "White"
+            },
+            {
+                variantId: 2,
+                variantColor: "Black"
+            }
+        ],
+        sizes: ["S", "M", "L", "XL", "XXL"]
     }
 })
